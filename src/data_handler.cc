546404a236bc48f880657da556ea9c666d27d5e6
enriquedlh97 <enriquedlh97@hotmail.com>
2021-07-15 19:09:40 -0500

    Fixes test main function

--- src/data_handler.cc
+++ src/data_handler.cc
@@ -202,8 +202,8 @@ std::vector<data *> *data_handler::get_validaiton_data()
 int main()
 {
     data_handler *dh = new data_handler();
-    dh->read_feature_vector("data/train-images-idx3-ubyte");
-    dh->read_feature_labels("data/train-labels-idx1-ubyte");
+    dh->read_feature_vector("./data/train-images-idx3-ubyte");
+    dh->read_feature_labels("./data/train-labels-idx1-ubyte");
     dh->split_data();
     dh->count_classes();
 }
